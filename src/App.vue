33f5163cbdaaf0963e8fb37532d85113eba153ec
Matias E Gonzalez <mgonzalez@temperies.com>
2019-04-24 13:02:25 -0300

    asignamet-resolved

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
-  <h1>Hello</h1>
+    <div class="container">
+        <div class="row">
+            <app-header></app-header>
+        </div>
+        <hr>
+        <div class="row">
+            <app-server></app-server>
+            <app-details></app-details>            
+        </div>
+        <hr>
+        <div class="row">
+            <app-footer></app-footer>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import Server from './Server.vue';
+    import ServerDetails from './ServerDetails.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-footer' : Footer,
+            'app-server': Server,
+            'app-details': ServerDetails
+        }
+    }
 
 </script>
 
---
+++ src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+       <server-status v-for="server in 5">{{ server }}</server-status>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './ServerStatus.vue';
+    export default {
+        components:{
+            'server-status': ServerStatus
+        }
+       
+    }
+</script>
+
+
+
+<style></style>
