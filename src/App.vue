b2a1dafc552527627ab081cf5f6b0c0dae47a2d8
Matias E Gonzalez <mgonzalez@temperies.com>
2019-05-08 12:25:50 -0300

    dinamic-component-an-keep-alive

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,20 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               <app-quote>
-                   <h2 slot="title">{{ quoteTitle }}</h2> <!-- only the style is setup in te child, others comprtamientos it's change here -->
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{selectedComponent}}</p>
+                <keep-alive>
+                    <component :is="selectedComponent"></component>
+                </keep-alive>
+                
+               <!-- <app-quote>
+                   <h2 slot="title">{{ quoteTitle }}</h2> 
                    <p>Somo quote by Slot</p>
-               </app-quote>
+               </app-quote> -->
+               <!-- only the style is setup in te child, others comprtamientos it's change here -->
             </div>
         </div>
     </div>
@@ -13,14 +23,19 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
     export default {
         data: function(){
             return{
-                quoteTitle: 'Somo title from data'
+                quoteTitle: 'Somo title from data',
+                selectedComponent: 'appQuote'
             }
         },
         components: {
-            appQuote: Quote
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
---
+++ src/components/New.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="counter++">incrase</button>
+        <h3>New Quote!</h3>
+        <p>{{counter}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function (){
+        return{
+            counter:0
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
