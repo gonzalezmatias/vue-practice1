7561cb540d43f3688a5358237bb7b0bf1abed96c
Matias E Gonzalez <mgonzalez@temperies.com>
2019-05-09 10:16:00 -0300

    display-some-quotes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="container">
-    
+        <app-grid :quotes="quotes"></app-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue'
     export default {
-        
+        data: function(){
+            return{
+                quotes: [
+                    'Example quote'
+                ],
+                maxQuotes: 10
+            } 
+        },
+        components:{
+            appGrid: QuoteGrid
+        }
     }
 </script>
 
---
+++ src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col-sm-6 col-md-4 col-lg-3">
+        <div class="panel panel-default">
+            <div class="panel-body quote">
+                <slot></slot>
+            </div>
+        </div>
+    
+    </div>
+</template>
+
+<script>
+    export default {
+      
+    }
+</script>
+
+<style>
+    .panel-body{
+        font-family: 'Arizonia', cursive;
+        font-size: 24px;
+        color: palegreen;
+    }
+
+    .quote{
+        cursor:pointer;
+    }
+
+    .quote:hover{
+        background-color: coral;
+    }
+</style>
