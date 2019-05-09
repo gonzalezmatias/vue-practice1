f2e850c7ab7ab36bcef86902e5d0fd4cffe17392
Matias E Gonzalez <mgonzalez@temperies.com>
2019-05-09 12:17:27 -0300

    NewQuotes-added

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -5,6 +5,9 @@
               <label>Quote</label>
               <textarea class="form-control" rows="3" v-model="quote"></textarea>  
             </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group"> 
+             <button class="btn btn-primary"></button>
+            </div>
         </form>
     </div>
 </template>
